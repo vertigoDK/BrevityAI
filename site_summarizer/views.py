a651674410073a3b22467b5d62bbbe4767fcc808
Denis Kushnaryov <denis.k.kushnaryov@gmail.com>
2024-10-28 21:57:21 +0500

    fix RPG

--- site_summarizer/views.py
+++ site_summarizer/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .services.site_summarizer import SiteSummarizer
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.decorators import login_required
 from .forms import SiteUrlForms
+from django.contrib import messages
 
 @login_required
 def site_summarizer_view(request: HttpRequest) -> HttpResponse:
@@ -11,20 +12,20 @@ def site_summarizer_view(request: HttpRequest) -> HttpResponse:
         form = SiteUrlForms(request.POST)
         
         if form.is_valid():
-            print('форма валидна')
             site_url = form.cleaned_data['site_url']
 
             siteSummarizer: SiteSummarizer = SiteSummarizer()
             
             response: dict[str, str] = siteSummarizer.summarize_site(link=site_url)
             
-            print(response)
+            request.session['response'] = response
             
-            return render(request=request, template_name="site_summarizer/site_summarizer.html", context={'response': response})
+            return redirect('site-summarizer')
         else:
-            print('Форма не валидна')
             print(form.errors)
     else:
         form = SiteUrlForms()
-        
-    return render(request=request, template_name="site_summarizer/site_summarizer.html")
+    
+    response = request.session.pop('response', None)
+    
+    return render(request=request, template_name="site_summarizer/site_summarizer.html", context={'form': form, 'response': response})
